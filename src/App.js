f2d59ad1d142a7e17779c9c1748e94a8eb0de696
faisalakonda <faisalakonda2@gmail.com>
2023-05-08 20:43:02 +0600

    Post Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
     </div>
   );
+}
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([])
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=> setPosts(data))
+  },[])
+  return (
+    <div>
+      <h2>Post: {posts.length}</h2>
+      <div className='div-style'>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+      </div>
+    </div>
+  );
+};
+function Post(props) {
+  return(
+    <div className='style-container'>
+        <h1>Title: {props.title}</h1>
+        <p>Body: {props.body}</p>
+      </div>
+  )
 }
 
 export default App;
